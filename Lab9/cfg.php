69e5c02d017df6f731cfcf8a5f67f2494eaf5522
GOstrowski04 <148046211+GOstrowski04@users.noreply.github.com>
2025-12-17 21:56:07 +0100

    Add files via upload

---
+++ Lab9/cfg.php
@@ -0,0 +1,17 @@
+<?php
+/// Dane potrzebne do połączenia z bazą danych
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$baza   = 'moja_strona';
+$login = 'admin';
+$pass = 'password';
+
+// Połączenie z bazą
+$link = mysqli_connect($dbhost, $dbuser, $dbpass, $baza);
+
+// Sprawdzenie połączenia
+if (!$link) {
+    die('<b>Przerwane połączenie z bazą danych</b>');
+}
+?>
